2c5b93536e5ed4964eb1e1c3593dfa8466d38901
Jakub Jagiello <kubajagiello94@gmail.com>
2018-09-06 12:07:08 +0200

    Added some tests and debugged queue

--- queue.c
+++ queue.c
@@ -1,8 +1,5 @@
 
-#include <lzma.h>
-#include <stdlib.h>
 #include "queue.h"
-#include <pthread.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
@@ -12,7 +9,7 @@ queue *queue_create(void) {
     queue* queue = calloc(1, sizeof(queue));
     queue->head = calloc(1, sizeof(node));
     queue->head->next = queue->head;
-    queue->head->prev = NULL;
+    queue->head->prev = queue->head;
     queue->head->value = NULL;
     queue->freeFunc = NULL;
     pthread_mutex_unlock(&mutex);
@@ -20,11 +17,11 @@ queue *queue_create(void) {
 }
 
 queue_position queue_dequeue(queue* queue) {
-    pthread_mutex_lock(&mutex);
-    while(queue_is_empty(queue)){
-        pthread_cond_wait(&cond, &mutex);
+    if(queue_is_empty(queue)){
+        return NULL;
     }
-    node *node_to_return = queue->head->prev;
+    pthread_mutex_lock(&mutex);
+    queue_position node_to_return = queue->head->prev;
     queue->head->prev = node_to_return->prev;
     node_to_return->prev->next = queue->head;
     node_to_return->next = NULL;
@@ -39,6 +36,7 @@ int queue_enqueue(queue* queue, data value) {
     new_node->value = value;
     queue_position last_node = queue->head->next;
     new_node->next = last_node;
+    new_node->prev = queue->head;
     last_node->prev = new_node;
     queue->head->next = new_node;
     pthread_mutex_unlock(&mutex);
